6641f5c0923b8843e698f39ceb931fd640fc90ef
Siddharth Bhat <siddu.druid@gmail.com>
2025-08-29 10:23:08 +0100

    chore: write evalator

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Nonlinear Hensel
 
+- [Notes on hensel when derivative is zero](https://math.stackexchange.com/questions/90692/hensels-lemma-fx-equiv-0-pmodp-case)
 
 ### Build instructions
 
--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ struct Robdd {
 #[derive(Debug, Clone)]
 struct Factor {
     // the truth table of the boolean function.
-    tt : TruthTable,
+    bool_fn : TruthTable,
     // the coefficient of the factor.
     coeff : i64,
 }
@@ -55,14 +55,15 @@ struct Literal {
 }
 
 #[derive(Debug, Clone)]
-struct Clause {
+struct ConjClause {
     vars: Vec<Literal>,
 }
 
 #[derive(Debug, Clone)]
-struct CNF {
-    clauses: Vec<Clause>,
+struct Dnf {
+    clauses: Vec<ConjClause>,
 }
+
 
 impl fmt::Display for Literal {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -74,17 +75,20 @@ impl fmt::Display for Literal {
     }
 }
 
-impl fmt::Display for Clause {
+impl fmt::Display for ConjClause {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let parts: Vec<String> = self.vars.iter().map(|lit| lit.to_string()).collect();
-        write!(f, "({})", parts.join("|"))
+        write!(f, "({})", parts.join("&"))
     }
 }
 
-impl fmt::Display for CNF {
+impl fmt::Display for Dnf {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let parts: Vec<String> = self.clauses.iter().map(|cl| cl.to_string()).collect();
-        write!(f, "{}", parts.join("&"))
+        if parts.is_empty() {
+            return write!(f, "(F)");
+        }
+        write!(f, "{}", parts.join("|"))
     }
 }
 
@@ -111,31 +115,31 @@ type BoolEnv = Env<bool>;
 type IntEnv = Env<i64>;
 
 impl BoolEnv {
-    fn to_clause(&self) -> Clause {
+    fn to_clause(&self) -> ConjClause {
         let mut vars = Vec::new();
         for (i, &val) in self.assigns.iter().enumerate() {
             vars.push(Literal { var: i, negated: !val });
         }
-        Clause { vars }
+        ConjClause { vars }
     }
 }
 
 impl IntEnv {
-    fn to_bool_env(&self, slice : usize) -> BoolEnv {
+    fn to_bool_env_by_slice_bits(&self, slice : usize) -> BoolEnv {
         let mut assigns = Vec::new();
         for i in 0..self.assigns.len() {
-            assigns.push((self.assigns[i] >> slice) & 1 == 1);
+            assigns.push((self.assigns[i] & (1 << slice)) == 1);
         }
         BoolEnv::new(assigns)
     }
 
-    fn of_bool_env(env: &BoolEnv, slice : usize) -> Self {
-        let mut assigns = Vec::new();
-        for i in 0..env.assigns.len() {
-            assigns.push((env.assigns[i] as i64) << i);
-        }
-        IntEnv::new(assigns)
-    }
+    // fn of_bool_env(env: &BoolEnv, slice : usize) -> Self {
+    //     let mut assigns = Vec::new();
+    //     for i in 0..env.assigns.len() {
+    //         assigns.push((env.assigns[i] as i64) << slice);
+    //     }
+    //     IntEnv::new(assigns)
+    // }
 }
 
 impl fmt::Display for IntEnv {
@@ -153,12 +157,13 @@ impl fmt::Display for IntEnv {
     }
 }
 
-impl BoolEnv {
-    fn to_int_env(&self) -> IntEnv {
-        IntEnv::of_bool_env(&self, 0)
-    }
 
-}
+// impl BoolEnv {
+//     fn to_int_env(&self) -> IntEnv {
+//         IntEnv::of_bool_env(self, 0)
+//     }
+//
+// }
 
 #[derive(PartialEq, Eq, Debug, Clone)]
 struct TruthTable {
@@ -172,14 +177,14 @@ fn truth_tables(nvars : usize) -> Vec<TruthTable> {
     let mut tables = Vec::new();
     let total_entries = 1 << nvars; // 2^nvars
     for i in 0..(1 << total_entries) { // 2^(2^nvars)
-        let mut tt = TruthTable::new();
+        let mut bool_fn = TruthTable::new();
         for j in 0..total_entries {
             if (i & (1 << j)) != 0 {
                 let assigns : Vec<bool> = (0..nvars).map(|k| (j & (1 << k)) != 0).collect();
-                tt.add_true(Env { assigns });
+                bool_fn.add_true(Env { assigns });
             }
         }
-        tables.push(tt);
+        tables.push(bool_fn);
     }
     tables
 }
@@ -188,47 +193,46 @@ impl TruthTable {
     fn new() -> Self {
         TruthTable { table: HashSet::new() }
     }
-    
+
     fn add_true(&mut self, entry: BoolEnv) {
         self.table.insert(entry);
     }
 
     fn eval_bool(&self, env: &BoolEnv) -> bool {
-        self.table.contains(&env)
+        self.table.contains(env)
     }
 
-    fn eval_int(&self, env: &IntEnv) -> i64 {
+    fn eval_int_bitwise(&self, env: &IntEnv) -> i64 {
         // iterate from [0, 64), take slices of 'BoolEnv',
         // evaluate and shift.
+        // eval bitwise
         let mut result = 0;
         for i in 0..64 {
-            let slice = env.to_bool_env(i);
+            let slice = env.to_bool_env_by_slice_bits(i);
             result |= (self.eval_bool(&slice) as i64) << i;
         }
         result
     }
 
-    fn to_cnf(&self) -> CNF {
-        CNF { clauses: self.table.iter().map(|env| env.to_clause()).collect() }
+    fn to_dnf(&self) -> Dnf {
+        Dnf { clauses: self.table.iter().map(|env| env.to_clause()).collect() }
     }
 
 }
 
 impl fmt::Display for TruthTable {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.to_cnf().fmt(f)
+        self.to_dnf().fmt(f)
     }
 }
 
 impl Factor {
-    fn new(tt: TruthTable, coeff: i64) -> Self {
-        Factor { tt, coeff }
+    fn new(bool_fn: TruthTable, coeff: i64) -> Self {
+        Factor { bool_fn, coeff }
     }
 
-    fn next_coeff(&self) -> Option<i64> {
-        // Generate the next coefficient in some order (e.g., incrementing)
-        // Placeholder implementation
-        None
+    fn eval_int(&self, env: &IntEnv) -> i64 {
+        self.coeff * self.bool_fn.eval_int_bitwise(env)
     }
 }
 
@@ -236,23 +240,15 @@ impl fmt::Display for Factor {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         // print +/- for coeff
         let sign = if self.coeff < 0 { "-" } else { "+" };
-        write!(f, "{}{}{}", sign, self.coeff.abs(), self.tt)
+        write!(f, "{}{}{}", sign, self.coeff.abs(), self.bool_fn)
     }
 }
 
 impl Term {
-    fn new() -> Self {
-        Term { factors: Vec::new() }
-    }
-
-    fn add_factor(&mut self, factor: Factor) {
-        self.factors.push(factor);
-    }
-
     fn eval_int(&self, env: &IntEnv) -> i64 {
         let mut result = 0;
         for factor in &self.factors {
-            let factor_value = factor.coeff * (factor.tt.eval_int(env) as i64);
+            let factor_value = factor.eval_int(env);
             result += factor_value;
         }
         result
@@ -264,6 +260,9 @@ impl fmt::Display for Term {
         // write separated by space.
         let mut entries: Vec<String> = self.factors.iter().map(|factor| factor.to_string()).collect();
         entries.sort();
+        if entries.is_empty() {
+            entries.push("0".to_string());
+        }
         write!(f, "{}", entries.join(" "))
     }
 }
@@ -271,7 +270,7 @@ impl fmt::Display for Term {
 // a Generatable<T> allows us to get first() and next() values of type T.
 
 struct Generator {
-    tts : Vec<TruthTable>,
+    bool_fns : Vec<TruthTable>,
     nvars : usize,
     maxcoeff : usize, // max coefficient value.
     maxfactors : usize, // max number of factors.
@@ -281,13 +280,13 @@ struct Generator {
 impl Generator {
 
     fn first_truth_table(&self) -> Option<&TruthTable> {
-        self.tts.first()
+        self.bool_fns.first()
     }
 
-    fn next_truth_table(&self, tt : &TruthTable) -> Option<TruthTable> {
-        let idx = self.tts.iter().position(|x| x == tt)?;
-        if idx + 1 < self.tts.len() {
-            Some(self.tts[idx + 1].clone())
+    fn next_truth_table(&self, bool_fn : &TruthTable) -> Option<TruthTable> {
+        let idx = self.bool_fns.iter().position(|x| x == bool_fn)?;
+        if idx + 1 < self.bool_fns.len() {
+            Some(self.bool_fns[idx + 1].clone())
         } else {
             None
         }
@@ -306,16 +305,16 @@ impl Generator {
     }
 
     fn first_factor(&self) -> Option<Factor> {
-        let tt = self.first_truth_table()?.clone();
+        let bool_fn = self.first_truth_table()?.clone();
         let coeff = self.first_coeff()?;
-        Some(Factor::new(tt, coeff))
+        Some(Factor::new(bool_fn, coeff))
     }
 
     fn next_factor(&self, factor: &Factor) -> Option<Factor> {
         match self.next_coeff(factor.coeff) {
-            Some(next_coeff) => Some(Factor::new(factor.tt.clone(), next_coeff)),
-            None => match self.next_truth_table(&factor.tt) {
-                Some(next_tt) => Some(Factor::new(next_tt, self.first_coeff()?)),
+            Some(next_coeff) => Some(Factor::new(factor.bool_fn.clone(), next_coeff)),
+            None => match self.next_truth_table(&factor.bool_fn) {
+                Some(next_bool_fn) => Some(Factor::new(next_bool_fn, self.first_coeff()?)),
                 None => None,
             },
         }
@@ -360,6 +359,7 @@ struct IntEnvIter {
     min_val: i64,
     max_val: i64,
     current: Vec<i64>,
+    done: bool,
 }
 
 impl IntEnvIter {
@@ -368,6 +368,7 @@ impl IntEnvIter {
             nvars,
             min_val,
             max_val,
+            done: false,
             current: vec![min_val; nvars],
         }
     }
@@ -377,61 +378,92 @@ impl IntEnvIter {
     }
 }
 
+// return if we could increment
+fn env_next(env: &mut Vec<i64>, min_val: i64, max_val: i64) -> bool {
+    for i in 0..env.len() {
+        if env[i] < max_val {
+            env[i] += 1;
+            for j in 0..i {
+                env[j] = min_val;
+            }
+            return true;
+        }
+    }
+    false
+}
 
 impl Iterator for IntEnvIter {
     type Item = Env<i64>;
 
     fn next(&mut self) -> Option<Self::Item> {
 
+        if self.done {
+            return None;
+        }
+
         // produce current environment
-        let env = Env {
+        let out = Env {
             assigns: self.current.clone(),
         };
 
         // increment like an odometro (odometer)
-        for i in (0..self.nvars).rev() {
-            if self.current[i] < self.max_val {
-                self.current[i] += 1;
-                for j in i+1..self.nvars {
-                    self.current[j] = self.min_val;
-                }
-                return Some(env);
-            }
-        }
-
-        // if we reach here, we are done
-        None
+        let did_increment = env_next(&mut self.current, self.min_val, self.max_val);
+        self.done = !did_increment;
+        Some(out)
     }
 }
 
-fn check_term(g : &Generator, t : &Term) {
+fn print_term_eval_table(t : &Term, iter : IntEnvIter) {
+    for env in iter {
+        let val = t.eval_int(&env);
+        let str = format!("   @ {} => {}", env, val);
+        let str_colored = if val == 0 {
+            str.dimmed().to_string()
+        } else {
+            str
+        };
+        println!("{}", str_colored);
+    }
+}
+
+enum CheckTermResult {
+    Good,
+    Skip(IntEnv),
+    Error(IntEnv),
+}
+
+fn check_term(g : &Generator, t : &Term) -> CheckTermResult {
     // check if eqn holds with inputs {0, 1}, and then check if it
     // holds for inputs being bitvectors where the relations hold.
     if let Some(cex) = t.is_identically_zero (IntEnvIter::new_bool(g.nvars)) {
         print!("{}:", "SKIP".yellow());
         println!("Term {} evaluates to nonzero value {} @ env {}",
             t, t.eval_int(&cex), cex);
-        return; 
+        print_term_eval_table(t, IntEnvIter::new_bool(g.nvars));
+        return CheckTermResult::Skip(cex)
     }
 
-    // because equation holds, check on larger outputs. 
+    // because equation holds, check on larger outputs.
     if let Some(cex) = t.is_identically_zero (IntEnvIter::new(g.nvars,
         -(g.maxvarval as i64),
         g.maxvarval as i64)) {
         print!("{}:", "ERROR".red());
         println!("Term {} that was identically zero on {{0,1}} evaluates to nonzero value {:?} @ env {}",
             t, t.eval_int(&cex), cex);
-        return; 
+
+        print_term_eval_table(t, IntEnvIter::new_bool(g.nvars));
+        CheckTermResult::Error(cex)
     } else {
         print!("{}:", "GOOD".green());
         println!("Term {} is identically zero both on booleans and bitvectors", t);
+        CheckTermResult::Good
     }
 
 }
 
 fn main() {
     let g = Generator {
-        tts: truth_tables(2),
+        bool_fns: truth_tables(2),
         nvars: 2,
         maxcoeff: 3,
         maxfactors: 2,
@@ -442,13 +474,37 @@ fn main() {
         panic!("No first term available")
     );
 
+    let mut errors = Vec::new();
+    let mut good = Vec::new();
+    let mut skips = Vec::new();
     loop {
-        check_term(&g, &t);
+        let check_result = check_term(&g, &t);
+        match check_result {
+            CheckTermResult::Good =>
+                good.push(t.clone()),
+            CheckTermResult::Skip(env) =>
+                skips.push((t.clone(), env)),
+            CheckTermResult::Error(env) => {
+                errors.push((t.clone(), env));
+                break;
+            },
+        }
+
         // get the next term.
         if let Some(next_term) = g.next_term(&t) {
             t = next_term;
         } else {
             break;
         }
+    }
+
+    println!("\nSummary:");
+    println!("  {} terms checked", good.len() + skips.len() + errors.len());
+    println!("  {} terms are identically zero", good.len());
+    println!("  {} terms skipped", skips.len());
+    println!("  {} terms found with errors", errors.len());
+
+    for (ter, env) in errors {
+        println!("Error term: {} @ env {} = {}", ter, env, ter.eval_int(&env));
     }
 }
